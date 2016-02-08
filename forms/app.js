fb3e5397871d6b4acf8243a1bce842e2d99765f4
Tim Adam <tdadam@xactware.com>
2016-02-08 10:28:55 -0700

    Class end

--- forms/app.js
+++ forms/app.js
@@ -11,4 +11,43 @@ angular.module('formExample', [])
         };
 
         $scope.reset();
-    }]);
+    }])
+
+    .directive('integer', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, controller) {
+                controller.$validators.integer = function (modelValue, viewValue){
+                    var INTEGER_REGEXP = /^[-+]?\d+$/;
+                    if (INTEGER_REGEXP.test(viewValue)) {
+                        return true;
+                    }
+                   return false;
+                };
+            }
+        };
+    })
+
+    .directive('myusername', function ($q, $timeout) {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, controller) {
+                var usernames = ["Mike", "Zac", "Wes", "Tyler", "Tim", "Kris"];
+                controller.$asyncValidators.myusername = function (modelValue, viewValue){
+                    if (controller.$isEmpty(modelValue)){
+                        return $q.when();
+                    }
+                    var def = $q.defer();
+                    $timeout(function() {
+                        if (usernames.indexOf(modelValue) == -1){
+                            def.resolve();
+                        }
+                        else {
+                            def.reject();
+                        }
+                    }, 2000);
+                    return def.promise;
+                };
+            }
+        };
+    });
